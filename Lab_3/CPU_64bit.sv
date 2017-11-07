3ebd0cdca4eb3278545327d89c0b9a855fed96ec
Emraj <emraj@uw.edu>
2017-11-07 01:00:28 -0800

    Tried working on the main cpu file. Little success

--- Lab_3/CPU_64bit.sv
+++ Lab_3/CPU_64bit.sv
@@ -1,5 +1,16 @@
 module CPU_64bit (clk, reset);
 	input logic clk, reset;
-	logic [63:0] address;
+	logic [63:0] ReadData1, ReadData2;
 	logic [31:0] instruction;
+	
+	//logic [4:0] selectRegister [1:0];
+	wire [4:0] Rd, Rm, Rn;
+	logic RegWrite;
+	//wire chooseRegister;
+	//assign Rd = selectRegister[0];
+	//assign Rm = selectRegister[1];
+	//logic Reg2Loc;
+	
+	//mux2_1 reg2locSignal(.in(selectRegister[1:0]), .sel(Reg2Loc), .out(chooseRegister));
+	regfile registerFile (.ReadData1, .ReadData2, .WriteData(Rd), .ReadRegister1(Rn), .ReadRegister2(Rm), .RegWrite, .clk);
 endmodule
